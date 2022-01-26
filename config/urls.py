628ce43989aa0919711669912008dcf53919536f
kkh5428@gmail.com <KaniKim>
2022-01-26 15:38:27 +0900

    add test set, add urls settings

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('pages.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    # Django Admin
+    path("admin/", admin.site.urls),
+    # User Management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("", include("pages.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
