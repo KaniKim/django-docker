a85f27d749aaa9d81147a3921671fc7b29f431d0
kkh5428@gmail.com <KaniKim>
2022-02-09 22:20:10 +0900

    add vue 3 and change to Dockerize

--- billing/views.py
+++ billing/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class Index(TemplateView):
+class Index(TemplateView, LoginRequiredMixin):
     template_name = "../vue_spa/public/index.html"
--- vue_spa/src/App.vue
+++ vue_spa/src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <div id="app">
-    <InvoiceCreate/>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view/>
 </template>
 
-<script>
-import InvoiceCreate from './components/InvoiceCreate.vue'
-
-export default {
-  name: 'App',
-  components: {
-    InvoiceCreate
-  }
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
