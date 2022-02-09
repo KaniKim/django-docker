65d1eb1f26de1b8b97de57eaf0ccf8ba404a662a
KaniKim <kkh5428@gmail.com>
2022-02-09 22:40:59 +0900

    delete prefetch from vue

--- vue_spa/vue.config.js
+++ vue_spa/vue.config.js
@@ -1,18 +1,24 @@
-const BundleTracker = require('webpack-bundle-tracker');
+const BundleTracker = require('webpack-bundle-tracker')
 
 module.exports = {
   publicPath: 'http://127.0.0.1:8080/',
   outputDir: './dist/',
 
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.optimization.splitChunks(false)
-
-    config.plugin('BundleTracker').use(BundleTracker, [{filename: './webpack-stats.json'}])
-
-    config.devServer.public('http://0.0.0.0:8080').host('0.0.0.0').port(8080).https(false).headers({"Access-Control-Allow-Origin":["\*"]})
+    config
+      .plugin('BundleTracker')
+      .use(BundleTracker, [{ filename: './webpack-stats.json' }])
+    config.devServer
+      .public('http://0.0.0.0:8080')
+      .host('0.0.0.0')
+      .port(8080)
+      .https(false)
+      .headers({ 'Access-Control-Allow-Origin': ['*'] })
+    config.plugins.delete('prefetch')
   },
 
   pages: {
-    index: 'src/main.js'
-  }
+    index: 'src/main.js',
+  },
 }
